dc8d419ee3a4f0449966c5fc68799d97e13550f2
Ventsislav Peychev <del.busque@gmail.com>
2022-06-01 18:01:43 +0300

    Added check for accessories availability

--- Cubicle/src/controllers/cubeAccessoryController.js
+++ Cubicle/src/controllers/cubeAccessoryController.js
@@ -1,10 +1,12 @@
 const router = require('express').Router({ mergeParams: true });
 const cubeService = require('../services/cubeService');
+const accessoryService = require('../services/accessoryService');
 
 router.get('/add', async (req, res) => {
 
-    let cube = await cubeService.getOne(req.params.cubeId)
-    res.render('cube/accessory/add', { ...cube })
+    let cube = await cubeService.getOne(req.params.cubeId);
+    let accessories = await accessoryService.getAll();
+    res.render('cube/accessory/add', { cube, accessories })
 })
 
 
--- Cubicle/src/services/accessoryService.js
+++ Cubicle/src/services/accessoryService.js
@@ -4,8 +4,13 @@ async function create(name, description, imageUrl) {
     return Accessory.create({ name, description, imageUrl })
 }
 
+async function getAll() {
+    return Accessory.find({}).lean();
+}
+
 const accessoryService = {
-    create
+    create,
+    getAll
 }
 
 //exports.create = create;
