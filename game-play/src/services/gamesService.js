f2792e2d402e8ce5e0f5f8c0ecdeda56e2aba71e
Ventsislav Peychev <del.busque@gmail.com>
2022-08-08 11:18:42 +0300

    Update gamesService.js

--- game-play/src/services/gamesService.js
+++ game-play/src/services/gamesService.js
@@ -1,3 +1,5 @@
+import * as request from "./requester";
+
 const baseUrl = 'http://localhost:3030'
 
-export const getAll = () => fetch(`${baseUrl}/data/games`).then(res => res.json());
+export const getAll = () => request.get(`${baseUrl}/data/games`);
