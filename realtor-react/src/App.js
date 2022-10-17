6f5c1802cb1de6be41fa6f4683a3fb661bed762f
Ventsislav Peychev <del.busque@gmail.com>
2022-10-17 21:37:18 +0300

    Added initial routes

--- realtor-react/src/App.js
+++ realtor-react/src/App.js
@@ -1,8 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Profile from './pages/Profile';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+
+
 function App() {
   return (
-    <>
-      <h1 className='text-2xl bg-red-400'>Hello World!</h1>
-    </>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/offers' element={<Offers />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
