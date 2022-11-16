63ad9c7c349f970525df3ee78700df01bdef55f7
delbusque <del.busque@gmail.com>
2022-11-16 12:40:10 +0200

    Updated apples state and firebase collection

--- aj-smits/src/App.js
+++ aj-smits/src/App.js
@@ -2,35 +2,18 @@ import './App.css';
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 
-import { useState, useEffect } from 'react';
-
 import Header from './components/header/Header';
 
 import Main from './components/main/Main';
 import Apples from './components/fruits/Apples';
 import Pears from './components/fruits/Pears';
 
-import Footer from './components/footer/Footer';
+// import Footer from './components/footer/Footer';
 import Register from './components/register/Register.js';
 import Login from './components/login/Login.js';
 
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore'
-import { firebaseConfig } from './firebaseConfig.js';
 
 function App() {
-
-  initializeApp(firebaseConfig);
-  const db = getFirestore();
-  const colRefApples = collection(db, 'apples');
-  const [apples, setApples] = useState([]);
-
-  useEffect(() => {
-    getDocs(colRefApples)
-      .then(snapshot => {
-        snapshot.docs.forEach(doc => setApples(oldState => [...oldState, { ...doc.data(), id: doc.id }]))
-      })
-  }, [])
 
   return (
     <div id="top" className="App">
@@ -39,14 +22,14 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Main />} />
-        <Route path='/fruit/apples' element={<Apples apples={apples} />} />
+        <Route path='/fruit/apples' element={<Apples />} />
         <Route path='/fruit/pears' element={<Pears />} />
 
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
       </Routes>
 
-      <Footer />
+      {/* <Footer /> */}
 
     </div>
 
