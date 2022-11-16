c30dcbdfd2e078436dd8b960a9818107dbda723b
delbusque <del.busque@gmail.com>
2022-11-16 17:03:55 +0200

    Add new navigation UI

--- aj-smits/src/components/header/Header.js
+++ aj-smits/src/components/header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './Header.module.css'
 
 import { Link } from 'react-router-dom';
 
@@ -6,38 +7,38 @@ const Header = () => {
     return (
         <div className="wrapper row0">
             <div id="topbar" className="hoc clear">
-                {/* ################################################################################################ */}
                 <div className="fl_left">
-                    <ul className="nospace">
-                        <li>
-                            <Link to="/">
-                                {/*<img src="/images/logo-miniAA.png" alt="" />*/}
-                            </Link>
-                        </li>
-
-                    </ul>
+                    <Link to="/">
+                        <img src="/images/logo-miniAA.png" alt="" />
+                    </Link>
                 </div>
                 <div className="fl_right">
                     <ul className="nospace">
-                        <li>
-                            <Link to="/">
-                                <i className="fas fa-home" />
+                        <li className={styles['nav']}>
+                            <Link to="/" title='Home'>
+                                <i className="fas fa-home" /><span> Home</span>
                             </Link>
                         </li>
 
-                        <li>
-                            <Link to="/register" title="Sign Up">
-                                <i className="fas fa-edit" />
+                        <li className={styles['nav']}>
+                            <Link to="/register" title="Register">
+                                <i className="fas fa-edit" /><span> Register</span>
                             </Link>
                         </li>
 
-                        <li>
+                        <li className={styles['nav']}>
                             <Link to="/login" title="Login">
-                                <i className="fas fa-sign-in-alt" />
+                                <i className="fas fa-sign-in-alt" /><span> Login</span>
                             </Link>
                         </li>
 
-                        <li id="searchform">
+                        <li className={styles['nav']}>
+                            <Link to="/logout" title="Logout">
+                                <i className="fas fa-sign-out-alt" /><span> Logout</span>
+                            </Link>
+                        </li>
+
+                        {/* <li id="searchform">
                             <div>
                                 <form action="#" method="post">
                                     <fieldset>
@@ -49,10 +50,9 @@ const Header = () => {
                                     </fieldset>
                                 </form>
                             </div>
-                        </li>
+                        </li> */}
                     </ul>
                 </div>
-                {/* ################################################################################################ */}
             </div>
         </div>
 
