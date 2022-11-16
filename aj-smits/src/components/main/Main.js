55cfa37b68aaeecd4f93bcaa3217d2d2811b21d9
delbusque <del.busque@gmail.com>
2022-11-16 12:34:10 +0200

    Update Main.js

--- aj-smits/src/components/main/Main.js
+++ aj-smits/src/components/main/Main.js
@@ -1,13 +1,7 @@
-import { Routes, Route } from 'react-router-dom';
 import React from 'react'
-import ReactDOM from 'react-dom'
 
 import TopSlider from "./slider/TopSlider";
 import IntroBlocks from './intro/IntroBlocks.js';
-import About from './about/About.js';
-
-import Pears from '../fruits/Pears';
-import Apples from '../fruits/Apples';
 
 const Main = () => {
     return (
