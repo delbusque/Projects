7bb013d7ed11f550646e48dfeb8ca2ab9e5f80b2
delbusque <del.busque@gmail.com>
2022-11-17 14:15:15 +0200

    Add route guards for auth and non auth users

--- aj-smits/src/components/header/Header.js
+++ aj-smits/src/components/header/Header.js
@@ -2,8 +2,11 @@ import React from 'react';
 import styles from './Header.module.css'
 
 import { Link } from 'react-router-dom';
+import { UserAuth } from '../../contexts/AuthContext.js';
 
 const Header = () => {
+    const { user } = UserAuth();
+
     return (
         <div className="wrapper row0">
             <div id="topbar" className="hoc clear">
@@ -20,23 +23,30 @@ const Header = () => {
                             </Link>
                         </li>
 
-                        <li className={styles['nav']}>
-                            <Link to="/register" title="Register">
-                                <i className="fas fa-edit" /><span> Register</span>
-                            </Link>
-                        </li>
+                        {!user && <>
+                            <li className={styles['nav']}>
+                                <Link to="/register" title="Register">
+                                    <i className="fas fa-edit" /><span> Register</span>
+                                </Link>
+                            </li>
+                            <li className={styles['nav']}>
+                                <Link to="/login" title="Login">
+                                    <i className="fas fa-sign-in-alt" /><span> Login</span>
+                                </Link>
+                            </li>
+                        </>}
 
-                        <li className={styles['nav']}>
-                            <Link to="/login" title="Login">
-                                <i className="fas fa-sign-in-alt" /><span> Login</span>
+                        {user && <>  <li className={styles['nav']}>
+                            <Link to="/account" title='My Account'>
+                                <i className="fas fa-user" /><span> My Account</span>
                             </Link>
                         </li>
-
-                        <li className={styles['nav']}>
-                            <Link to="/logout" title="Logout">
-                                <i className="fas fa-sign-out-alt" /><span> Logout</span>
-                            </Link>
-                        </li>
+                            <li className={styles['nav']}>
+                                <Link to="/logout" title="Logout">
+                                    <i className="fas fa-sign-out-alt" /><span> Logout</span>
+                                </Link>
+                            </li>
+                        </>}
 
                         {/* <li id="searchform">
                             <div>
