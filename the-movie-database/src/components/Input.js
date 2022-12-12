f4b3bbfe1b1e897887653691fe4b0d2612adf4b8
delbusque <del.busque@gmail.com>
2022-12-12 23:30:13 +0200

    Add titles state from file upload in Input component

--- the-movie-database/src/components/Input.js
+++ the-movie-database/src/components/Input.js
@@ -2,25 +2,24 @@ import { useState } from 'react';
 import styles from './Input.module.css'
 
 export const Input = () => {
-    const [fileText, setFileText] = useState('');
+    const [titles, setTitles] = useState([]);
 
     const onChange = (e) => {
         const file = e.target.files[0];
         const reader = new FileReader();
         reader.onload = function (e) {
-            setFileText(e.target.result)
+            setTitles(JSON.parse(e.target.result));
         };
         reader.readAsText(file);
     }
 
+    console.log(titles);
+
     return (
         <div className={styles['upload-file']}>
-
+            <label htmlFor="inputfile">Works only with a JSON array structure </label>
             <input type="file" name="inputfile"
                 id="inputfile" onChange={(e) => onChange(e)} />
-
-            <div className={styles['text-output']}>{fileText}</div>
-
         </div>
     )
 }
