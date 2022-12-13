7837bb55c9154043941f4991b6edc7a8fc0627eb
delbusque <del.busque@gmail.com>
2022-12-13 19:53:43 +0200

    Update App.js

--- the-movie-database/src/App.js
+++ the-movie-database/src/App.js
@@ -1,11 +1,41 @@
+import { useState } from 'react';
+
 import './App.css';
+
+import TitlesContext from './contexts/TitlesContext.js';
+import SelectedTitlesContext from './contexts/SelectedTitlesContext.js';
+
 import { Input } from './components/Input.js';
+import { Titles } from './components/Titles.js';
+
 
 function App() {
+
+  const [titles, setTitles] = useState([]);
+  const [selectedTitles, setSelectedTitles] = useState([]);
+
+  const onChangeHandler = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      setTitles(JSON.parse(e.target.result));
+    };
+    reader.readAsText(file);
+  }
+
+  console.log(titles);
+  console.log(selectedTitles);
+
   return (
-    <div className="App">
-      <Input />
-    </div>
+    <TitlesContext.Provider value={{ titles, setTitles, onChangeHandler }}>
+      <SelectedTitlesContext.Provider value={{ selectedTitles, setSelectedTitles }}>
+        <div className="App">
+          <Input />
+          {titles.length > 0 && <Titles />}
+
+        </div>
+      </SelectedTitlesContext.Provider>
+    </TitlesContext.Provider>
   );
 }
 
