65d2a0c6fe1837c09c3ffdf130e454c68a0708ba
delbusque <del.busque@gmail.com>
2022-12-13 19:53:10 +0200

    Lift onChangeHandler of file input to App component

--- the-movie-database/src/components/Input.js
+++ the-movie-database/src/components/Input.js
@@ -1,25 +1,18 @@
-import { useState } from 'react';
+import { useContext } from 'react';
+import TitlesContext from '../contexts/TitlesContext.js';
 import styles from './Input.module.css'
 
 export const Input = () => {
-    const [titles, setTitles] = useState([]);
 
-    const onChange = (e) => {
-        const file = e.target.files[0];
-        const reader = new FileReader();
-        reader.onload = function (e) {
-            setTitles(JSON.parse(e.target.result));
-        };
-        reader.readAsText(file);
-    }
-
-    console.log(titles);
+    const { onChangeHandler } = useContext(TitlesContext);
 
     return (
-        <div className={styles['upload-file']}>
-            <label htmlFor="inputfile">Works only with a JSON array structures </label>
-            <input type="file" name="inputfile"
-                id="inputfile" onChange={(e) => onChange(e)} />
-        </div>
+        <>
+            <div className={styles['upload-file']}>
+                <label htmlFor="inputfile">Works only with a JSON array structures </label>
+                <input type="file" name="inputfile"
+                    id="inputfile" onChange={(e) => onChangeHandler(e)} />
+            </div>
+        </>
     )
 }
