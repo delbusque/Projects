1cf18f2d41104f066ae2a240c833cff15ad760e1
delbusque <del.busque@gmail.com>
2022-12-13 19:54:01 +0200

    Add titles and selected titles contexts

---
+++ the-movie-database/src/contexts/SelectedTitlesContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const SelectedTitlesContext = createContext();
+
+export default SelectedTitlesContext;
---
+++ the-movie-database/src/contexts/TitlesContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const TitlesContext = createContext();
+
+export default TitlesContext;
