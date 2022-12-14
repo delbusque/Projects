bf8d0c77a713bcc0d9efae50554ab7d02576226d
delbusque <del.busque@gmail.com>
2022-12-14 12:21:16 +0200

    Update Titles.js

--- the-movie-database/src/components/Titles.js
+++ the-movie-database/src/components/Titles.js
@@ -2,12 +2,13 @@ import { useContext } from "react"
 import TitlesContext from "../contexts/TitlesContext.js"
 import SelectedTitlesContext from "../contexts/SelectedTitlesContext.js"
 import { MovieTitle } from "./MovieTitle.js"
-import { Button } from './Button.js';
+import { Button } from './Buttons/Button.js';
 
 export const Titles = () => {
 
     const { titles } = useContext(TitlesContext);
     const { setSelectedTitles } = useContext(SelectedTitlesContext);
+
 
     const onSubmit = (e) => {
         e.preventDefault();
@@ -16,11 +17,9 @@ export const Titles = () => {
     }
 
     return (
+
         <form className="input-container" onSubmit={onSubmit}>
-
-
             {titles.map((t, i) => <MovieTitle key={t} title={t} index={i} />)}
-
 
             <Button />
         </form>
