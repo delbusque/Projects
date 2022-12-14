7e69471df9a25f7bb882533f564d1dd62899643b
delbusque <del.busque@gmail.com>
2022-12-14 12:21:54 +0200

    Add api utils

---
+++ the-movie-database/src/fetchUtils.js
@@ -0,0 +1,4 @@
+export const API_KEY = 'a30d22b75ae63291709718fef5139410';
+export const API_SEARCH = 'https://api.themoviedb.org/3/search';
+export const API_LIST = 'https://api.themoviedb.org/3/list';
+export const API_IMAGE = 'https://image.tmdb.org/t/p/w500';
