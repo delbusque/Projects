2ce6bcb6cb33396f07d8643d917237089d5e5a4a
delbusque <del.busque@gmail.com>
2022-12-15 00:10:39 +0200

    Update MovieTitle.js

--- the-movie-database/src/components/MovieTitle.js
+++ the-movie-database/src/components/MovieTitle.js
@@ -2,7 +2,7 @@ export const MovieTitle = ({ title, index }) => {
     return (
         <div className="title-container">
             <label htmlFor={`movie-${index}`} className="title">{title}</label>
-            <input className="checkbox" type="checkbox" id={`movie-${index}`} name="title" value={title} />
+            <input className="check-title" type="checkbox" id={`movie-${index}`} name="title" value={title} />
         </div>
     )
 }
