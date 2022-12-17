b9e5ed774c1d73fc013dc212b041b413a9f343d5
Ventsislav Peychev <del.busque@gmail.com>
2022-12-17 09:49:26 +0200

    Rename placeholder

--- the-movie-database/src/components/FetchedMovies.js
+++ the-movie-database/src/components/FetchedMovies.js
@@ -78,7 +78,7 @@ export const FetchedMovies = () => {
         <>
             <div className='theatre'>TMDB MOVIES LIST</div>
             <form className="search-bar" onSubmit={onSearch}>
-                <input type="text" name="query" placeholder="Indiana Jones and the Temple of Doom" className="search-input" onChange={(e) => setQuery(e.target.value)} />
+                <input type="text" name="query" placeholder="Indiana Jones" className="search-input" onChange={(e) => setQuery(e.target.value)} />
 
                 <button className="btn-search">Search</button>
             </form>
