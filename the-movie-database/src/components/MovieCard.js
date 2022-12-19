ee5cf7ca404a22a13b9b869d55637e824ec2b6bd
delbusque <del.busque@gmail.com>
2022-12-19 10:08:23 +0200

    Fixed media queries on movie card

--- the-movie-database/src/components/MovieCard.js
+++ the-movie-database/src/components/MovieCard.js
@@ -8,14 +8,12 @@ export const MovieCard = ({ movie }) => {
                 <div className="movie-card">
                     <img className="card-image" src={API_IMAGE + movie.backdrop_path} alt={movie.original_title + ' image'} />
                     <div className="card-info" >
-                        {movie.original_title ? <h2 className="card-title">{movie.original_title}</h2>
-                            : <h2 className="card-title">{movie.original_name}</h2>}
-                        <p className="card-overview">{movie.overview.slice(0, 200)}..</p>
-                        {movie.release_date ? <p className="card-date">- {movie.release_date.slice(0, 4)} -</p>
-                            : <p className="card-date">- {movie.first_air_date?.slice(0, 4)} -</p>}
+                        <h2 className="card-title">{movie.original_title}</h2>
+                        <p className="card-overview">{movie.overview.slice(0, 180)}..</p>
+                        <p className="card-date">- {movie.release_date.slice(0, 4)} -</p>
 
                     </div>
-                    <div>
+                    <div className="checkbox-cont">
                         <input className="card-checkbox" type="checkbox" id={`movie-${movie.id}`} name="movie"
                             value={movie.id} />
                     </div>
