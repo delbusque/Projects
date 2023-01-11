ed1bf0db2b91d77b694b96dfab0bd6160f2a4d1d
delbusque <del.busque@gmail.com>
2023-01-11 13:24:30 +0200

    Add Drink mongoose model

---
+++ restaurant-orders/backend/models/Drink.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const drinkSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Drink', drinkSchema);
